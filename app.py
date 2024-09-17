507a3ac3bd1f1e4759c4d2e2115c3a7f565fb37b
Darin755 <darinrawson@gmail.com>
2024-09-17 15:21:10 -0600

    added time

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flaski
+import datetime
+
 app = Flask(__name__)
+
+now = datetime.datetime.now()
 
 @app.route("/")
 def hello():
-    return "<h1>Hello World!</h1>"
+    return "<h1>Hello World! The time is "+now.time()+" on the server</h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
