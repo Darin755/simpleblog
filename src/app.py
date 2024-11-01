daa49b7abcf40283d47843cc2900da22feb868f3
Darin755 <darinrawson@gmail.com>
2024-11-01 09:50:21 -0600

    added button

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
 
 app = Flask(__name__, static_url_path='/static')
@@ -7,3 +7,13 @@ app = Flask(__name__, static_url_path='/static')
 @app.route("/")
 def hello_world():
     return render_template("demo.html")
+
+@app.route('/save', methods=['POST'])
+def handle_post():
+    if request.method == 'POST':
+        body = request.json
+        print(body)
+        return "saved"
+
+if __name__ == '__main__':
+    app.run()
