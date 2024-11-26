5ec9111940f45257f345576aa87e56b025f928b5
Darin755 <darinrawson@gmail.com>
2024-11-26 09:34:37 -0700

    added bootstrap and cleaned up

--- src/app.py
+++ src/app.py
@@ -1,11 +1,15 @@
 # Route for handling the login page logic
 from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap
 import os
 import fileprocessing
 import secrets
 import time
 
+print("starting up")
+
 app = Flask(__name__, static_url_path='/static')
+bootstrap = Bootstrap(app)
 
 # (user, key, time created)
 authorized_cookies = []
@@ -18,8 +22,11 @@ def checkAuth(cookie):
         for c in authorized_cookies:
             if cookie == c[1]:
                 auth = c[0]
+                print("authenticated "+auth+" with cookie")
                 break
     return auth #return user
+
+print("creating routes")
 
 #root route
 @app.route("/")
@@ -33,10 +40,11 @@ def root():
         auth = checkAuth(cookie)
         if not auth == False:
             #we are signed in
-            return render_template('demo.html')
+            return render_template('dash.html')
         else:
             #login please
             return redirect("/login", code=302)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -52,10 +60,12 @@ def login():
                 elif(result == 2):
                     errorReturned = 'Invalid Credentials. Please try again.'
                 elif(result == 0):
+                    print("authentication successful for "+request.form['username'])
                     authkey = str(secrets.randbits(512))
                     authorized_cookies.append((request.form['username'], authkey, time.time()))
                     resp = redirect("/", code=302)
                     resp.set_cookie('authcookie', authkey)
+                    print("set cookie for "+request.form['username'])
                     return resp
 
                 return render_template('login.html', errorReturned=errorReturned)
@@ -65,16 +75,19 @@ def login():
         else:
             return redirect("/newuser", code=302)
     else:
+        print("logged in")
         return redirect("/", code=302)
 
 @app.route('/logout', methods=['GET', 'POST'])
 def logout():
     #find cookie and then delete it
+    print("logging out")
     cookie = request.cookies.get('authcookie')
     if cookie:
         for i,c in enumerate(authorized_cookies):
             if c[1] == cookie:
                 del(authorized_cookies[i])
+                print("logged out")
     return redirect("/login", code=302)
 
 
@@ -82,6 +95,7 @@ def logout():
 def newuser():
     #only allow user creation if no users exist
     if fileprocessing.noUsers():
+        print("prompting to create new user")
         if request.method == 'POST':
             result = fileprocessing.newUser(request.form['username2'], request.form['password2'], request.form['passwordCheck'])
             if (result == 1):
@@ -100,9 +114,12 @@ def newuser():
 @app.route('/save', methods=['POST'])
 def handle_post():
     if request.method == 'POST':
-        body = request.json
-        print(body)
-        return "saved"
+        cookie = request.cookies.get('authcookie')
+        auth = checkAuth(cookie)
+        if not auth == False:
+            body = request.json
+            print(body)
+            return "saved"
 
 if __name__ == '__main__':
     app.run()
